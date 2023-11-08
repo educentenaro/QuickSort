2e40a6266f7426a97d5b9ec308986a827264ce79
Eduardo Centenaro <centenaro57@gmail.com>
2023-11-08 14:43:44 -0300

    Change to make equal numbers possible

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function ordenarNumeros() {
 
   console.log(`Executado em ${executionTime} milissegundos.`);
 }
-
+/*
 function quickSort(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -37,4 +37,28 @@ function quickSort(arr) {
   }
 
   return [...quickSort(left), pivot, ...quickSort(right)];
+}
+*/
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const equal = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else if (arr[i] > pivot) {
+      right.push(arr[i]);
+    } else {
+      equal.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), ...equal, ...quickSort(right)];
 }
